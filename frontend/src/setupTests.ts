cd46d8dfe469b5103f7603e2f19fa4fd4873ba74
Jesse Stover <j.stover.r@gmail.com>
2026-01-02 14:27:31 -0600

    feat: add Jest setup file for testing environment
    - Introduce `setupTests.ts` to configure Jest with testing library matchers and global test setup.

---
+++ frontend/src/setupTests.ts
@@ -0,0 +1,9 @@
+/**
+ * Jest setup file for test environment
+ *
+ * Configures testing library matchers and global test setup
+ */
+
+import "@testing-library/jest-dom";
+
+// Add any global test setup here
